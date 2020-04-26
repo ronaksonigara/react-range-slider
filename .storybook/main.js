78bbb205e4dc8d0dfb33f93a7942fa0d7e45666e
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2020-04-26 15:01:35 +0530

    story book knobs order changed

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
   stories: ["../stories/**/*.stories.js"],
   addons: [
+    "@storybook/addon-knobs/register",
     "@storybook/addon-actions",
     "@storybook/addon-links",
-    "@storybook/addon-knobs/register",
   ],
   webpackFinal: async (config) => {
     // do mutation to the config
